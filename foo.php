3e802b2af98c239583b701781ad51e3b36b0edfe
Kęstutis Vaškevičius <forkik@gmail.com>
2012-10-08 19:41:55 +0300

    Nfq akademija, Kęstutis Vaškevičius, Pradedam

---
+++ foo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kestutis
+ * Date: 08/10/12
+ * Time: 19:40
+ * To change this template use File | Settings | File Templates.
+ */
+echo 'Go to work';
